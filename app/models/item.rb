5af194d8f7b25dff9b22ebe2e248e285067952d5
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-04-19 10:49:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,19 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :information
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }, numericality: true
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :items_condition_id
+    validates :delivery_charge_id
+    validates :shipping_area_id
+    validates :shipping_date_id
+  end
 end
