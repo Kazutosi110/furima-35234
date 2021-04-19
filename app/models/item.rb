760bc0a5c0d0aa56bb0f00fac71d46e11a32fc66
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-04-19 09:57:49 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :items_condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one_attached :image
 end
