aef56af52ce087b260009ef115d64ba38907b1b7
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-04-22 16:11:24 +0900

    フォームの情報を保存

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,14 +2,15 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @record_purchase = RecordPurchase.new
     @item = Item.find(params[:item_id])
+    @record_purchase = RecordPurchase.new
   end
 
   def new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @record_purchase = RecordPurchase.new(record_params)
     if @record_purchase.valid?
       @record_purchase.save
@@ -22,7 +23,7 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record_purchase).permit(:delivery_municipality, :delivery_address, :postal_code, :shipping_area_id, :delivery_phone, :delivery_building, :items_id).merge(user_id: current_user.id)
+    params.require(:record_purchase).permit(:delivery_municipality, :delivery_address, :delivery_postcode, :shipping_area_id, :delivery_phone, :delivery_building).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
--- app/models/record_purchase.rb
+++ app/models/record_purchase.rb
@@ -1,19 +1,19 @@
 class RecordPurchase
   include ActiveModel::Model
-  attr_accessor :user_id, :items_id, :delivery_postcode, :shipping_area_id, :delivery_municipality, :delivery_address, :delivery_building, :delivery_phone
+  attr_accessor :user_id, :item_id, :delivery_postcode, :shipping_area_id, :delivery_municipality, :delivery_address, :delivery_building, :delivery_phone
 
   with_options presence: true do
     validates :user_id
-    validates :items_id
+    validates :item_id
     validates :delivery_municipality
     validates :delivery_address
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :delivery_postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :shipping_area_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :delivery_phone, format: { with: /\A0[0-9]{11}+\z/, message: 'number is invalid. Include half-width numbers' } 
+    validates :delivery_phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
   end
 
   def save
     record = Record.create(user_id: user_id, item_id: item_id)
-    Purchase.create(delivery_municipality: delivery_municipality, delivery_address: delivery_address, postal_code: postal_code, shipping_area_id: shipping_area_id, delivery_phone: delivery_phone, delivery_building: delivery_building, record_id: record.id)
+    Purchase.create(delivery_municipality: delivery_municipality, delivery_address: delivery_address, delivery_postcode: delivery_postcode, shipping_area_id: shipping_area_id, delivery_phone: delivery_phone, delivery_building: delivery_building, record_id: record.id)
   end
 end
