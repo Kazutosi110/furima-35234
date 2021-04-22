2b70896ad7dd66a158fd6d2936e3bc6dbb50434b
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-04-22 11:50:09 +0900

    コントローラーの編集

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,27 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!, except: :index
+
   def index
+    @record_purchase = RecordPurchase.new
   end
-  
+
   def new
   end
 
   def create
+    @record_purchase = RecordPurchase.new(record_params)
+    if @record_purchase.valid?
+      @record_purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def record_params
+    params.require(:record_purchase).permit(:delivery_municipality, :delivery_address, :postal_code, :shipping_area_id, :delivery_phone, :delivery_building, :items_id).merge(user_id: current_user.id)
+  end
+
 end
